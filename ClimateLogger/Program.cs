8e9ab5a23442fb843aa592e91aae33774b64e72a
Michał Maciejewski <tmplt@tmplt.io>
2024-11-24 16:53:02 +0100

    Services creation in Main

--- ClimateLogger/Program.cs
+++ ClimateLogger/Program.cs
@@ -1,7 +1,33 @@
-﻿namespace ClimateLogger;
+﻿using ClimateLogger.Configuration;
+using ClimateLogger.Data;
+using ClimateLogger.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ClimateLogger;
 
 internal static class Program
 {
-    public static void Main()
-    { }
+    public static async Task Main(string[] args)
+    {
+        var host = Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) =>
+            {
+                services.AddDbContext<Context>(options => options.UseSqlite(Manager.DatabaseLocation));
+                services.AddHostedService<MigrationService>();
+                services.AddHostedService<ClimateLoggerService>();
+            })
+            .ConfigureLogging(builder => builder.SetMinimumLevel(Manager.LoggerLevel))
+            .Build();
+
+        try
+        {
+            await host.RunAsync();
+        }
+        catch
+        { }
+    }
 }
